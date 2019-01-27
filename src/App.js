935bfdff520cdfa5289ae2885f7d2638a15f7ad9
Prosper <onungwap@gmail.com>
2019-01-27 15:21:06 -0500

    dynamic lists

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from "./Person/Person";
+
 class App extends Component {
+  state = {
+    persons: [
+      { id: 'asfaj', name: 'Max', age: 28},
+      { id: 'onkew', name: 'Obi', age: 53},
+      { id: 'urnqo', name: 'Ewo', age: 3}
+    ],
+    showPersons: false 
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({
+      persons : persons
+    });
+
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons:persons}); 
+
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons =  null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              click={() => this.deletePersonHandler(index)}
+              changed={(event) => this.nameChangedHandler(event, person.id)}>
+          </Person>
+          })}
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, This is React</h1>
-        <Person />
+        <button 
+        style={style}
+        onClick={this.togglePersonsHandler}>
+        Toggle Persons</button>
+
+        {persons}        
       </div>
     );
   }
